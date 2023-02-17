6b98ee0d9be58793da58a146eceb658ba0f2b880
Petra Orosz <oroszpetra11@gmail.com>
2023-02-17 18:01:29 +0100

    feat(assignment): vertical chart demo

--- src/app/charts/charts.module.ts
+++ src/app/charts/charts.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { VerticalComponent } from './vertical/vertical.component';
 import { HorizontalComponent } from './horizontal/horizontal.component';
 import { ChartsComponent } from './charts.component';
+import { NgChartsConfiguration, NgChartsModule } from 'ng2-charts';
 
 
 
@@ -16,7 +17,11 @@ import { ChartsComponent } from './charts.component';
     ChartsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgChartsModule
+  ],
+  providers: [
+    { provide: NgChartsConfiguration, useValue: { generateColors: false }}
   ]
 })
 export class ChartsModule { }
--- src/app/charts/horizontal/horizontal.component.ts
+++ src/app/charts/horizontal/horizontal.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-horizontal',
@@ -6,23 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./horizontal.component.scss']
 })
 export class HorizontalComponent {
-  config = {
-    type: 'bar',
-    data: {
-      datasets: [{
-        barPercentage: 0.5,
-        barThickness: 6,
-        maxBarThickness: 8,
-        minBarLength: 2,
-        data: [10, 20, 30, 40, 50, 60, 70]
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10
       }
     },
+    plugins: {
+      legend: {
+        display: true,
+      },
+    },
   };
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [];
+
+  public barChartData: ChartData<'bar'> = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+    ]
+  };
+
+  // events
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      Math.round(Math.random() * 100),
+      56,
+      Math.round(Math.random() * 100),
+      40 ];
+
+    this.chart?.update();
+  }
 }
