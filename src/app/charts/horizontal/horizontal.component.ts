dc79237a745dbb1fc57f9e0f4bc20e9f05326a45
Petra Orosz <oroszpetra11@gmail.com>
2023-02-17 17:07:37 +0100

    feat(assignment): import chartjs, not yet working

--- src/app/charts/horizontal/horizontal.component.ts
+++ src/app/charts/horizontal/horizontal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-horizontal',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./horizontal.component.scss']
 })
 export class HorizontalComponent {
+  config = {
+    type: 'bar',
+    data: {
+      datasets: [{
+        barPercentage: 0.5,
+        barThickness: 6,
+        maxBarThickness: 8,
+        minBarLength: 2,
+        data: [10, 20, 30, 40, 50, 60, 70]
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
 
+  // @ts-ignore
+  chart = new Chart("myChart", this.config);
 }
